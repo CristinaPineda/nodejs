37b47a2f3e520731a8d6fca93db1932e1d43c597
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-11 17:20:50 -0300

    teste de inclusao de argumentos e leitura

--- argumentos/index.js
+++ argumentos/index.js
@@ -8,3 +8,9 @@ console.log(args);
 
 const nome = args[0].split('=')[1];
 console.log(nome);
+
+// executar no terminal node index.js nome=Cristina idade=24
+const idade = args[1].split('=')[1];
+console.log(idade);
+
+console.log(`Meu nome é ${nome} eu tenho ${idade} anos de idade.`);
