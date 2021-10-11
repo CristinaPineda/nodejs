21923c4496fdafeca082056dcba6d613709dd297
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-11 20:41:50 -0300

    programa linha de comando

--- pratica-args/index.js
+++ pratica-args/index.js
@@ -1,5 +1,12 @@
+//externo
 const minimist = require('minimist');
 
-const args = minimist(process.argv.slice(2));
-console.log(args);
+//interno
+const soma = require('./soma').soma;
 
+const args = minimist(process.argv.slice(2));
+
+const a = parseInt(args['a']);
+const b = parseInt(args['b']);
+
+soma(a, b);
