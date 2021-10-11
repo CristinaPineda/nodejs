56c3c266d57bb27ef480bf03cfcd4f17131bc5d7
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-11 15:08:26 -0300

    ajuste de funcao

--- simpleNode/core-module/index.js
+++ simpleNode/core-module/index.js
@@ -1,9 +1,10 @@
-const fs = require('fs'); // file system
+const fs = require('fs'); // file system (arquivos de texto)
 
 fs.readFile('arquivo.txt', 'utf8', (err, data) => {
 
   if(err){
     console.log(err)
+    return
   }
   console.log(data)
 
