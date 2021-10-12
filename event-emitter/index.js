ab335a1d9c80e828fe384ab41557ce8421138e54
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-11 22:38:30 -0300

    comentarios sobre execucao

--- event-emitter/index.js
+++ event-emitter/index.js
@@ -5,8 +5,8 @@ eventEmitter.on('start', () => {
   console.log('Durante');
 });
 
-console.log('Antes');
+console.log('Antes'); // 01. lê essa linha e imprime primeiro
 
-eventEmitter.emit('start');
+eventEmitter.emit('start'); // 02. dá o comando que a linha 4 está esperando e executa a função anonima que estava esperando o "start"
 
-console.log('Depois');
+console.log('Depois'); // 03. lê e imprime essa linha após tudo.
