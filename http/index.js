d7d4191de9b04e6844c182bef20969f70ff16ee5
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-25 15:42:24 -0300

    hello world

---
+++ http/index.js
@@ -0,0 +1,14 @@
+//http 
+
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.write('hello world')
+  res.end()
+})
+
+server.listen(port,() => {
+  console.log(`Servidor rodando na porta: ${port}`)
+})
