bc849db990a486f02aa85fcbe8f6ac01789331ef
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-25 22:27:33 -0300

    alguns usos do path

---
+++ path/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const customPath = "projetos/cristina/projeto01.pdf";
+
+console.log(path.dirname(customPath));
+console.log(path.basename(customPath));
+console.log(path.extname(customPath));
