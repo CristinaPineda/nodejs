12d89d7b65aa3a35b5c61e0d07121aed4701e5df
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-04 18:40:07 -0300

    usando handlebars

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.get('/',(req, res) => {
+  res.render('home', { layout: false })
+})
 
+app.listen(3000, () => {
+  console.log('Aplicação rodando na porta: 3000' )
+})
