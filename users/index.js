a9937b3a734278160b72bc2f2e42e7411a9044a2
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-04 15:36:49 -0300

    cria rotas

--- users/index.js
+++ users/index.js
@@ -4,3 +4,10 @@ const router = express.Router();
 const path = require("path");
 const basePath = path.join(__dirname, "../templates");
 
+router.use('/cats', (_req, res) => {
+  res.sendFile(`${basePath}/cats.html`)
+});
+
+router.use('/dogs', (_req, res) => {
+  res.sendFile(`${basePath}/dogs.html`)
+});
