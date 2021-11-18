4ebc59d96f8bf1e38b34881b7aaef208823d9605
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-17 23:28:28 -0300

    config exprexx pra pegar o body

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const MYSQLPASS = process.env.MYSQLPASS;
 
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json());
+
 const hbs = exphbs.create();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -15,6 +23,10 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.post('/books/inserbook', (req, res) => {
+
 })
 
 const connect = mysql.createConnection({
