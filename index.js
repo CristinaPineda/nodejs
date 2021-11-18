c713c22e36d4167657471aeba1f8fa026759fe69
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-17 23:26:01 -0300

    ajusta erro no static

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const hbs = exphbs.create();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(express.static('public/index.html'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('home')
